62a8dbed958510e03bb2aed4e50bf3758c82d400
Ans1337 <answaheed1@gmail.com>
2022-08-14 00:18:01 +0500

    initial comit to github

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,38 @@
+//imports
+//main function
+//calling of main functions
+
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const chainId = network.config.chainId
+
+    //const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    const FundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress], //
+        log: true,
+    })
+    log("-------------------------------------------------")
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+    }
+}
+
+module.exports.tags = ["all", "fundme"]
